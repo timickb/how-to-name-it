1f7f1dcabd891a5e79410ab59cc81ae7af6faf9a
timickb <timikm2002@gmail.com>
2021-04-24 16:53:47 +0300

    Kacheli: создана связь между тем, какое задание какой скилл прокачивает и сколько конкретно очков прибавляет к этому скиллу.

---
+++ Database/Kacheli/ActivityWeight.cs
@@ -0,0 +1,16 @@
+﻿namespace Database.Kacheli
+{
+    /// <summary>
+    /// Данная сущность устанавливает связь между
+    /// тем, какое задание какой скилл прокачивает
+    /// и сколько конкретно очков прибавляет к этому скиллу.
+    /// </summary>
+    public class ActivityWeight
+    {
+        public virtual Activity Activity { get; set; }
+        
+        public virtual Skill Skill { get; set; }
+        
+        public int Points { get; set; }
+    }
+}
