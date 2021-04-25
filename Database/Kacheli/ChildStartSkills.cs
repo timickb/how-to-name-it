27e326e97698e108a036c13083ecf2e64db01a67
timickb <timikm2002@gmail.com>
2021-04-25 10:12:03 +0300

    Kacheli: ChildStartSkills

---
+++ Database/Kacheli/ChildStartSkills.cs
@@ -0,0 +1,16 @@
+﻿namespace Database.Kacheli
+{
+    /// <summary>
+    /// Сущность описывает начальное количество
+    /// баллов прокачки определенного скилла
+    /// у определенного ребенка.
+    /// </summary>
+    public class ChildStartSkills
+    {
+        public virtual ChildUser Child { get; set; }
+        
+        public virtual Skill Skill { get; set; }
+        
+        public int Points { get; set; }
+    }
+}
